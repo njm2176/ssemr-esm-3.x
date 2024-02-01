82754ba2e403573b23a93c0eac2c53bb05cce618
PiusKariuki <piuskariuki19@gmail.com>
2024-02-01 11:33:02 +0300

    Loading states for stat cards

--- packages/esm-patient-hiv-art-app/src/home-dashboard/components/cards/StatCard.tsx
+++ packages/esm-patient-hiv-art-app/src/home-dashboard/components/cards/StatCard.tsx
@@ -1,19 +1,28 @@
 import React, { useEffect } from "react";
 import styles from "./index.scss";
 import { useFetch } from "../../../hooks/useFetch";
+import { Loading } from "@carbon/react";
 
 const StatCard = ({ item }) => {
   const { loading, error, makeRequest, data } = useFetch();
 
   useEffect(() => {
-    if (item) makeRequest(item.url);
+    if (item?.url) makeRequest(item.url);
   }, [item]);
 
   return (
     <div className={styles.card}>
       <div className={styles.title}>{item.title}</div>
-      <p className={styles.stat}>{data?.entry?.length || 0}</p>
-      {item.icon}
+      {loading ? (
+        <div className={styles.spinner}>
+          <Loading withOverlay={false} small={true} />
+        </div>
+      ) : (
+        <>
+          <p className={styles.stat}>{data?.entry?.length || 0}</p>
+          {item.icon}
+        </>
+      )}
     </div>
   );
 };
