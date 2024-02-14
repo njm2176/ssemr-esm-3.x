4419bacac42baf16a91ad681d20cc6a6098a979e
pius <piuskariuki19@gmail.com>
2024-02-14 20:59:28 +0300

    Third chart

--- packages/esm-patient-hiv-art-app/src/home-dashboard/charts/ActiveClients.tsx
+++ packages/esm-patient-hiv-art-app/src/home-dashboard/charts/ActiveClients.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { PieChart } from "@carbon/charts-react";
 import "@carbon/charts-react/styles.css";
 import { activeClientData } from "../dummy/data";
+import { useHomeDashboard } from "../hooks/useHomeDashboard";
 
 const ActiveClients = () => {
+  const { activeClients, getDummyData, allClients } = useHomeDashboard();
+
+  const formatData = () => {
+    return [
+      {
+        group: "Inactive",
+        value: allClients?.results?.length - activeClients?.results?.length,
+      },
+      {
+        group: "Active",
+        value: activeClients?.results?.length,
+      },
+    ];
+  };
+
+  useEffect(() => {
+    formatData();
+  }, []);
+
   const options = {
     title: "Active Clients",
     resizable: true,
     height: "400px",
   };
-  return <PieChart data={activeClientData} options={options}></PieChart>;
+  return <PieChart data={formatData()} options={options}></PieChart>;
 };
 
 export default ActiveClients;
