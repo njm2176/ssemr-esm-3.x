53396d75b429a3e4c4c42c7bb90ec463946f3978
pius <piuskariuki19@gmail.com>
2024-02-14 13:27:58 +0300

    Second line chart

--- packages/esm-patient-hiv-art-app/src/home-dashboard/charts/CurrentArtClients.tsx
+++ packages/esm-patient-hiv-art-app/src/home-dashboard/charts/CurrentArtClients.tsx
@@ -1,16 +1,47 @@
 import React, { useEffect } from "react";
 import { currentARTClients } from "../dummy/data";
-import { AreaChart } from "@carbon/charts-react";
+import { AreaChart, LineChart } from "@carbon/charts-react";
 import "@carbon/charts-react/styles.css";
 import "./index.scss";
+import { useHomeDashboard } from "../hooks/useHomeDashboard";
 
 const NewlyEnrolled = () => {
+  const { activeClients } = useHomeDashboard();
+
+  const formatSummary = () => {
+    const monthsArray = Object.keys(activeClients?.summary?.groupYear);
+
+    const formattedData = monthsArray.map((month) => ({
+      month: month,
+      clients: activeClients.summary.groupYear[month],
+    }));
+
+    return formattedData;
+  };
+
+  const options = {
+    title: "Newly Enrolled Clients",
+    axes: {
+      bottom: {
+        title: "Months",
+        mapsTo: "month",
+        scaleType: "labels",
+      },
+      left: {
+        title: " Number of clients",
+        mapsTo: "clients",
+        scaleType: "linear",
+      },
+    },
+    curve: "curveMonotoneX",
+    height: "400px",
+  };
+
   return (
     <div className="">
-      <AreaChart
-        data={currentARTClients.data}
-        options={currentARTClients.options}
-      />
+      {activeClients?.summary && (
+        <LineChart data={formatSummary()} options={options} />
+      )}
     </div>
   );
 };
