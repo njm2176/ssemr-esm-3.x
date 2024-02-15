3df049da192d7728f2febbf08fb94baf927df642
pius <piuskariuki19@gmail.com>
2024-02-15 08:27:51 +0300

    removed swr in useFetch.tsx

--- packages/esm-patient-hiv-art-app/src/hooks/useFetch.tsx
+++ packages/esm-patient-hiv-art-app/src/hooks/useFetch.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { openmrsFetch } from "@openmrs/esm-framework";
-import useSWR from "swr";
 
 export const useFetch = () => {
   const [loading, setLoading] = useState(false);
@@ -11,7 +10,6 @@ export const useFetch = () => {
     try {
       setLoading(true);
       const response = await openmrsFetch(url);
-      const { data, error, isLoading } = useSWR(url, openmrsFetch, {});
       setData(data);
       onResult(data, null);
     } catch (error) {
