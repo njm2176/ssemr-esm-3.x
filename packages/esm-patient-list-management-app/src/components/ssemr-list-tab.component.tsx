62a45c3127425fc3f6019fd44e89d5109e10085b
pius <piuskariuki19@gmail.com>
2024-06-19 12:21:41 +0300

    (chore) Client listing styling

---
+++ packages/esm-patient-list-management-app/src/components/ssemr-list-tab.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./index.scss";
+
+// const SsemrListTabComponent = ({
+//   name,
+//   handler,
+//   activeClassNam,
+//   className,
+// }) => {
+//   return <div className={}></div>;
+// };
+
+// export default SsemrListTabComponent;
