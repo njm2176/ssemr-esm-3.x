fd4c320edab6c94deeee424e3c8d82a57684dd05
pius <piuskariuki19@gmail.com>
2024-06-19 12:37:21 +0300

    (feat) Tabs on client listing

--- packages/esm-patient-list-management-app/src/components/ssemr-list-tab.component.tsx
+++ packages/esm-patient-list-management-app/src/components/ssemr-list-tab.component.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./index.scss";
 
-// const SsemrListTabComponent = ({
-//   name,
-//   handler,
-//   activeClassNam,
-//   className,
-// }) => {
-//   return <div className={}></div>;
-// };
+const SsemrListTabComponent = ({ name, handler, isActive }) => {
+  return (
+    <button
+      className={isActive ? styles.activeTab : styles.tab}
+      onClick={handler}
+    >
+      {name}
+    </button>
+  );
+};
 
-// export default SsemrListTabComponent;
+export default SsemrListTabComponent;
