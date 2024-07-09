470769086cbd7eab350cdbdf25792fa8e63a520f
pius <piuskariuki19@gmail.com>
2024-07-09 12:38:32 +0300

    chore: renamed the route and path from  "Downloadable Patient Summary" to "Downloadable Client Summary"

--- packages/esm-patient-summary-app/src/dashboard.meta.ts
+++ packages/esm-patient-summary-app/src/dashboard.meta.ts
@@ -1,6 +1,6 @@
 export const dashboardMeta = {
   slot: "patient-chart-care-panel-dashboard-slot",
   columns: 1,
-  title: "Downloadable Patient Summary",
-  path: "Downloadable Patient Summary",
+  title: "Downloadable Client Summary",
+  path: "Downloadable Client Summary",
 };
