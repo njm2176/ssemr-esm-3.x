5b30d5a34f52eab0fde437e9d99020d8e3640271
pius <piuskariuki19@gmail.com>
2024-09-11 22:55:29 +0300

    chore: import optimization

--- packages/esm-patient-hiv-art-app/src/components/tabs/chart-selector-tabs.component.tsx
+++ packages/esm-patient-hiv-art-app/src/components/tabs/chart-selector-tabs.component.tsx
@@ -22,11 +22,12 @@ const ChartSelectorTabsComponent = () => {
   ];
 
   return (
-    <Tabs selectedIndex={tabs.findIndex(tab=> location.pathname === tab.path)}>
+    <Tabs
+      selectedIndex={tabs.findIndex((tab) => location.pathname === tab.path)}
+    >
       <TabList aria-label="List of tabs">
         {tabs.map((tab, index) => (
           <Tab
-            // selectedIndex={location.pathname === tab.path}
             selectedIndex={location.pathname === tab.path}
             onClick={() => navigate(tab.path)}
             key={tab.path}
