1e084e334008ab56f58061bfd7abd006dc45c419
Samuel Muli <samuelmuli95@gmail.com>
2024-09-23 22:58:58 +0300

    fix the tests

--- packages/esm-patient-list-management-app/src/components/tab/ssemr-list-tab.test.tsx
+++ packages/esm-patient-list-management-app/src/components/tab/ssemr-list-tab.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import '@testing-library/jest-dom';
 import SsemrListTabComponent from "./ssemr-list-tab.component";
 
 describe("Tab Components", () => {
--- packages/esm-patient-list-management-app/src/views/client-lists.test.tsx
+++ packages/esm-patient-list-management-app/src/views/client-lists.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { screen, render } from "@testing-library/react";
-import '@testing-library/jest-dom';
 import ListsDashboard from "./client-lists.component";
 import { usePatientListing } from "../hooks/usePatientListing";
 
