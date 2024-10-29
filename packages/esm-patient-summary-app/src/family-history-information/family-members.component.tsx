e4420956ef4323ac35e700abe0e922c20f93eaf0
Your Name <michaelndula@gmail.com>
2024-10-29 10:47:42 +0300

    Update Patient Summary ESM

--- packages/esm-patient-summary-app/src/family-history-information/family-members.component.tsx
+++ packages/esm-patient-summary-app/src/family-history-information/family-members.component.tsx
@@ -36,67 +36,7 @@ const FamilyHistory: React.FC<FamilyHistoryProps> = ({ code }) => {
   useEffect(() => {
     getPatientUuid();
   }, []);
-  //   try {
-  //     setLoading(true);
 
-<<<<<<< HEAD
-  //     const {
-  //       data: { results },
-  //     } = await axios.get(
-  //       `/openmrs/ws/rest/v1/obs?patient=${patientUuid}&_sort=-date&limit=100`
-  //     );
-  //     const generalInfoArray = [];
-
-  //     for (let i = 0; i < results.length - 1; i++) {
-  //       if (results[i].display.includes("General Family Information")) {
-  //         generalInfoArray.push({
-  //           display: results[i].display.split(":")[1],
-  //           index: i,
-  //         });
-  //       }
-  //     }
-
-  //     for (let i = 0; i < generalInfoArray.length - 1; i++) {
-  //       const subArray = results.slice(generalInfoArray[i].index + 1);
-  //       const valuesArray = generalInfoArray[i].display.split(",");
-  //       const valueObject = {};
-
-  //       for (let j = 0; j < valuesArray.length - 1; j++) {
-  //         const matchingItem = subArray.find((item) =>
-  //           item.display.includes(valuesArray[j])
-  //         );
-
-  //         valueObject[matchingItem.display.split(":")[0]] =
-  //           matchingItem.display.split(":")[1];
-
-  //         setColumns((prevState) =>
-  //           prevState.some(
-  //             (item) => item.name === matchingItem.display.split(":")[0]
-  //           )
-  //             ? prevState
-  //             : [
-  //                 ...prevState,
-  //                 {
-  //                   name: matchingItem.display.split(":")[0],
-  //                   selector: (row) => row[matchingItem.display.split(":")[0]],
-  //                 },
-  //               ]
-  //         );
-  //       }
-  //       setRows((prev) => [...prev, valueObject]);
-  //     }
-
-  //     const formattedData = results.filter((item) =>
-  //       item.display.includes("General Family Information")
-  //     );
-
-  //     setPatientData(formattedData);
-  //   } catch (e) {
-  //     console.error("error", e);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
 
   const getObservationData = () => {
     if (data && data.results && data.results.length > 0) {
@@ -106,16 +46,6 @@ const FamilyHistory: React.FC<FamilyHistoryProps> = ({ code }) => {
         (result) => result.familyMembers || []
       )
 
-=======
-  const getObservationData = () => {
-    if (data && data.results && data.results.length > 0) {
-      setLoading(true)
-
-      const allFamilyMembers = data.results.flatMap(
-        (result) => result.familyMembers || []
-      )
-
->>>>>>> 5dc1603 (Solve conflicts)
       if(allFamilyMembers.length > 0) {
         setColumns(defaultFamilyTableHeaders)
 
