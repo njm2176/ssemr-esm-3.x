85742cc559b1e95e9e179d93bf8e38bd665a016f
Your Name <michaelndula@gmail.com>
2025-06-12 10:49:45 +0300

    feat(data-management): Optimize client-side data fetching with SWR to improve system perfomance

--- packages/esm-patient-summary-app/src/hooks/useObservationData.ts
+++ packages/esm-patient-summary-app/src/hooks/useObservationData.ts
@@ -1,112 +1,31 @@
-import { useState, useEffect } from "react";
+import useSWR from 'swr';
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return response.json();
+};
 
 const useObservationData = (patientUuid) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const swrKey = patientUuid ? `/openmrs/ws/rest/v1/ssemr/dashboard/obs?patientUuid=${patientUuid}` : null;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          `/openmrs/ws/rest/v1/ssemr/dashboard/obs?patientUuid=${patientUuid}`
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        setError(error.message);
-      }
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [patientUuid]);
-
-  const defaultFamilyTableHeaders = [
-    {
-      name: "Name",
-      selector: (row) => row.name || "---",
-    },
-    {
-      name: "Age",
-      selector: (row) => row.age || "---",
-    },
-    {
-      name: "Sex",
-      selector: (row) => row.sex || "---",
-    },
-    {
-      name: "HIV Status",
-      selector: (row) => row.hivStatus || "---",
-    },
-    {
-      name: "Unique ART No.",
-      selector: (row) => row.artNumber || "---",
-    },
-  ];
-
-  const defaultIndexTableHeaders = [
-    {
-      name: "Name",
-      selector: (row) => row.name || "---",
-    },
-    {
-      name: "Age",
-      selector: (row) => row.age || "---",
-    },
-    {
-      name: "Sex",
-      selector: (row) => row.sex || "---",
-    },
-    {
-      name: "Relationship",
-      selector: (row) => row.relationship || "---",
-    },
-    {
-      name: "HIV Status",
-      selector: (row) => row.hivStatus || "---",
-    },
-    {
-      name: "Phone No.",
-      selector: (row) => row.phone || "---",
-    },
-    {
-      name: "Unique ART No.",
-      selector: (row) => row.uniqueArtNumber || "---",
-    },
-  ];
-
-  const defaultCHWHeaders = [
-    {
-      name: "Cadre",
-      selector: (row) => row.cadre || "---",
-    },
-    {
-      name: "Name",
-      selector: (row) => row.name || "---",
-    },
-    {
-      name: "Phone",
-      selector: (row) => row.phone || "---",
-    },
-    {
-      name: "Address",
-      selector: (row) => row.address || "---",
-    }
-  ]
+  const { data, error, isLoading, mutate } = useSWR(swrKey, fetcher, {
+    revalidateOnFocus: true, 
+    revalidateOnReconnect: true,
+    // refreshInterval: 5000,                             
+    // onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+    //   if (retryCount >= 3) return;
+    //   setTimeout(() => revalidate(), 5000);
+    // }
+  });
 
   return {
     data,
     isLoading,
-    setIsLoading,
     error,
-    defaultFamilyTableHeaders,
-    defaultIndexTableHeaders,
-    defaultCHWHeaders,
+    refetch: mutate,
   };
 };
 
