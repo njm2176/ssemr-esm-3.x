40740b26b28f3621c057cec3567420cc127aadbd
Gift <yatagift96@gmail.com>
2025-09-16 11:37:11 +0200

    SSEMR-668 A dashboard chart is added that displays the total number of completed EAC sessions.

---
+++ packages/esm-patient-hiv-art-app/src/components/chart-sections/stacked-bar/mockData.ts
@@ -0,0 +1,77 @@
+export const mockData = [
+  {
+    group: "EAC 1",
+    key: "Jan",
+    value: 65000,
+  },
+  {
+    group: "EAC 1",
+    key: "Feb",
+    value: 29123,
+  },
+  {
+    group: "EAC 1",
+    key: "Mar",
+    value: 35213,
+  },
+  {
+    group: "EAC 1",
+    key: "Apr",
+    value: 51213,
+  },
+  {
+    group: "EAC 1",
+    key: "May",
+    value: 16932,
+  },
+  {
+    group: "EAC 2",
+    key: "Jan",
+    value: 32432,
+  },
+  {
+    group: "EAC 2",
+    key: "Feb",
+    value: 21312,
+  },
+  {
+    group: "EAC 2",
+    key: "Mar",
+    value: 56456,
+  },
+  {
+    group: "EAC 2",
+    key: "Apr",
+    value: 21312,
+  },
+  {
+    group: "EAC 2",
+    key: "May",
+    value: 34234,
+  },
+  {
+    group: "EAC 3",
+    key: "Jan",
+    value: 12312,
+  },
+  {
+    group: "EAC 3",
+    key: "Feb",
+    value: 23232,
+  },
+  {
+    group: "EAC 3",
+    key: "Mar",
+    value: 34232,
+  },
+  {
+    group: "EAC 3",
+    key: "Apr",
+    value: 12312,
+  },
+  {
+    group: "EAC 3",
+    key: "May",
+    value: 84234,
+  },
+];
