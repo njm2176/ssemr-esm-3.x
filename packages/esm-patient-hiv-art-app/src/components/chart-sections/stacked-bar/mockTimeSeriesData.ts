df348fc03123c377920e13f59696c1df2ae85765
Jansuk Emmanuel <jansukemma1900@gmail.com>
2025-09-16 15:26:09 +0200

    SSEMR-669 The chart should support filtering by date range (e.g., monthly, quarterly, yearly).

---
+++ packages/esm-patient-hiv-art-app/src/components/chart-sections/stacked-bar/mockTimeSeriesData.ts
@@ -0,0 +1,11 @@
+export const mockTimeSeriesData = [
+  { group: "EAC 1", key: "2025-01-15", value: 65000 },
+  { group: "EAC 1", key: "2025-02-15", value: 29123 },
+  { group: "EAC 1", key: "2025-03-15", value: 35213 },
+  { group: "EAC 2", key: "2025-01-15", value: 32432 },
+  { group: "EAC 2", key: "2025-02-15", value: 21312 },
+  { group: "EAC 2", key: "2025-03-15", value: 56456 },
+  { group: "EAC 3", key: "2025-01-15", value: 12312 },
+  { group: "EAC 3", key: "2025-02-15", value: 23232 },
+  { group: "EAC 3", key: "2025-03-15", value: 34232 },
+];
